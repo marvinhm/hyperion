eddb09235e2ba256cf30dbbeee2ec2e0f1fc4560
marvinhm <marvinmatovu@gmail.com>
2023-02-21 22:33:19 +0000

    navbar styles + adding fonts

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import studentLogo from '../assets/student.png'
 
 function Navbar() {
   return (
@@ -7,18 +8,27 @@ function Navbar() {
         <div className='logo'> 
 
         </div>
-        <div className='nav-title mobile'>
+        <div className='nav-title mobile-title'>
           <h1>Get mentor support</h1>
         </div>
-        <div className='nav-title desktop'>
+        <div className='nav-title desktop-title'>
           <h1>Full Stack Web Developer Bootcamp</h1>
         </div>
       </div>
-
-      <div className='right'>
-        <div className=''>
-          User
+      
+      <div className='dropdown'>
+        <div className='right dropdown-toggle' data-toggle="dropdown" id="dropdownMenu1">
+          <div className='user-img'>
+            <img src={studentLogo} alt="..." class="img-circle"/>
+          </div>
+          <div className='user-profile-link'>
+          </div>
+          <p>Profile</p>
         </div>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Edit Profile</a></li>
+          <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Log out</a></li>
+        </ul>
       </div>
     </div>
   )
